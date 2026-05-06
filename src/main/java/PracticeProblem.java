f6263ce66b635657e1466fc50f2128f4106a052f
Stephen-Li-ICD2O1-01 <stephen.li@student.tdsb.on.ca>
2026-05-06 12:43:03 -0400

    Update PracticeProblem.java

--- src/main/java/PracticeProblem.java
+++ src/main/java/PracticeProblem.java
@@ -1,7 +1,17 @@
+/*
+File: Lesson 5.6 - Parallel arrays
+Author: Stephen Li
+Date Created: May 6, 2026
+Date Last Modified: May 6, 2026
+*/
+
 public class PracticeProblem {
 
 	public static void main(String args[]) {
 
 	}
 
+	public static String getAge(String[] names, int[] ages, String name) {
+		
+	}
 }
