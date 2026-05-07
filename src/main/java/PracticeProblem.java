1d2e081165899ed350f11d1f09fd4bf2e49f57a3
Stephen-Li-ICD2O1-01 <stephen.li@student.tdsb.on.ca>
2026-05-07 15:31:01 +0000

    save

--- src/main/java/PracticeProblem.java
+++ src/main/java/PracticeProblem.java
@@ -2,7 +2,7 @@
 File: Lesson 5.6 - Parallel arrays
 Author: Stephen Li
 Date Created: May 6, 2026
-Date Last Modified: May 6, 2026
+Date Last Modified: May 7, 2026
 */
 
 public class PracticeProblem {
@@ -12,6 +12,18 @@ public class PracticeProblem {
 	}
 
 	public static String getAge(String[] names, int[] ages, String name) {
-		
+		for (int i = 0; i < names.length; i++) {
+			if (name.equals(names[i])) {
+				return name;
+			}
+		}
+		return name;
+	}
+
+	public static String createLengthArray(String[] array1) {
+		int[] array2 = new int[array1.length];
+		for (int i = 0; i < array1.length; i++) {
+			
+		}
 	}
 }
